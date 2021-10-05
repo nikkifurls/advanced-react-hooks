60b36dfbc4c85afa236b95f454b021d857498401
Nicole Furlan <nfurlan@alley.co>
2021-10-04 20:19:54 -0400

    Exercise 1 extra credit 1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,13 +4,13 @@
 import * as React from 'react'
 
 function countReducer(previousCount, newCount) {
-  return newCount;
+  return previousCount + newCount;
 }
 
 function Counter({initialCount = 0, step = 1}) {
   const [count, setCount] = React.useReducer(countReducer, initialCount);
 
-  const increment = () => setCount(count + step)
+  const increment = () => setCount(step)
   return <button onClick={increment}>{count}</button>
 }
 
